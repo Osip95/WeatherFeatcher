5ee017ad4c36a7ccf218c356e2b80884ef461bfa
”Osipov <osss151095@gmail.com>
2023-01-21 17:20:54 +0300

    -inital commit

---
+++ app/src/main/java/com/example/weatherfetcher/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.weatherfetcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tvHello = findViewById<TextView>(R.id.tvHello)
+        tvHello.text = "Hello from code"
+    }
+}
