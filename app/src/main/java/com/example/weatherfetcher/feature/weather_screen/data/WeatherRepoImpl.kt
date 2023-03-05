2c42b174d60e07e745bd8895605989ee18e3f3d3
”Osipov <osss151095@gmail.com>
2023-03-05 17:57:05 +0300

    -удалил лишние комментарии

--- app/src/main/java/com/example/weatherfetcher/feature/weather_screen/data/WeatherRepoImpl.kt
+++ app/src/main/java/com/example/weatherfetcher/feature/weather_screen/data/WeatherRepoImpl.kt
@@ -1,10 +1,12 @@
 package com.example.weatherfetcher.feature.weather_screen.data
 
-import com.example.weatherfetcher.feature.weather_screen.data.model.Weather
 import com.example.weatherfetcher.feature.weather_screen.domain.WeatherRepo
+import com.example.weatherfetcher.feature.weather_screen.ui.model.WeatherModel
 
-class WeatherRepoImpl(private val weatherRemouteSource: WeatherRemouteSource): WeatherRepo {
+class WeatherRepoImpl(private val weatherRemouteSource: WeatherRemouteSource) : WeatherRepo {
 
-    override suspend fun getWeather(city: String): Weather = weatherRemouteSource.getWeather(city)
+    override suspend fun getWeather(city: String): WeatherModel = weatherRemouteSource
+        .getWeather(city)
+        .toDomain()
 
 }
--- app/src/main/java/com/example/weatherfetcher/feature/weather_screen/ui/StringConstants.kt
+++
@@ -1,3 +0,0 @@
-package com.example.weatherfetcher.feature.weather_screen.ui
-
-const val CITY: String = "city"
